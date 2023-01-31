c3f73af7b9b7a4760e0112766c36cf5a40d8c558
C0mput3-tech <113675010+C0mput3-tech@users.noreply.github.com>
2023-01-31 23:24:08 +0900

    Update 백준1992번-쿼드트리.cpp

--- 백준1992번-쿼드트리.cpp
+++ 백준1992번-쿼드트리.cpp
@@ -1,19 +1,13 @@
 #include<iostream>
 #include<string>
 #include<algorithm>
-
 using namespace std;
 int ss;
 #define time ios::sync_with_stdio(0),cin.tie(0);
-
 string qd(string it[100], int x, int y, int size)
 {
 	bool t = true;
 	char c = it[y][x];
-	if (size <= 1)
-	{
-		return string(1,c);
-	}
 	string ul;
 	string ur;
 	string dl;
@@ -27,24 +21,13 @@ string qd(string it[100], int x, int y, int size)
 					ul = qd(it, x , y, size / 2);
 					ur = qd(it, x + size / 2, y, size / 2);
 					dl = qd(it, x, y + size / 2, size / 2);
-					dr = qd(it, x + size / 2, y + size / 2, size / 2);
-					t = false;
-					break;
-					
+					dr = qd(it, x + size / 2, y + size / 2, size / 2);					
+					return string("(") + ul + ur + dl + dr + string(")");
 				}
-			}
-			if (t==false)
-			{
-				break;
-			}
-	}
-		if (t == true)
-		{
+			}	
+	}	
 			return string(1, c);
-	}
-		return string("(") + ul + ur + dl + dr + string(")");
 }
-
 int main() {
 	string ss[100];
 	int k;
